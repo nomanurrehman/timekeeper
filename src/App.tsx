5dc1ac55647f9766a8d3c59482b97b5674974d5a
Noman Ur Rehman <nomanurrehman@gmail.com>
2021-11-09 21:08:58 +0100

    Update app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux';
+import { Container, Grid } from '@mui/material';
+import { store } from './store';
+import { CreateTaskForm } from './containers/CreateTaskForm';
+import { DataTable } from './containers/DataTable';
+import { CustomTheme } from './components/CustomTheme';
+
+import { init } from "@rematch/core";
+init();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      {/* <CustomTheme> */}
+      <Container className="App" maxWidth="xl">
+        <Grid container={true} spacing={2}>
+          <CreateTaskForm />
+          <DataTable />
+        </Grid>
+      </Container>
+      {/* </CustomTheme> */}
+    </Provider>  
   );
 }
 
